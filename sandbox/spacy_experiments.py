dc350e93dae7f61700418cd1324dfe21d9983a72
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2019-04-21 11:24:57 -0400

    conditional lemmatization and nsubjpass

--- sandbox/spacy_experiments.py
+++ sandbox/spacy_experiments.py
@@ -4,13 +4,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 nlp = spacy.load("en_core_web_sm")
+important_dependencies = ['nsubj', 'dobj', 'nsubjpass']
+
+def simplify(token):
+    if token.pos_ == "VERB":
+        return token.lemma_
+    return token.text
 
 def test(src, gen):
     src = nlp(src)
     gen = nlp(gen)
     dependencies = dict()
     for token in src:
-        word, head, dep = token.text, token.head.text, token.dep_
+        word, head, dep = simplify(token), simplify(token.head), token.dep_
         if dep not in dependencies:
             dependencies[dep] = dict()
         if word not in dependencies[dep]:
@@ -19,8 +25,8 @@ def test(src, gen):
     contained = 0
     total = 0
     for token in gen:
-        word, head, dep = token.text, token.head.text, token.dep_
-        if not (token.dep_ == 'nsubj' or token.dep_ == 'dobj'):
+        word, head, dep = simplify(token), simplify(token.head), token.dep_
+        if dep not in important_dependencies:
             continue
         if dep in dependencies:
             if word in dependencies[dep]:
@@ -29,7 +35,7 @@ def test(src, gen):
                 else:
                     print("missing |", word, head, dep, "|", dependencies[dep][word])
             else:
-                print("missing |", word, head, dep, "| word not in source document")
+                print("missing |", word, head, dep, "| word not in source document under this dependency")
         else:
             print("missing |", word, head, dep, "| dependency not in source document")
     if total == 0:
@@ -58,7 +64,7 @@ with open("data/test.txt.src.tagged.shuf.400words") as src:
             print("summary:", gen_line)
             scores.append(test(src_line, gen_line))
             i += 1
-            if i == 10:
+            if i == 5:
                 break
 
 # sns.set()
