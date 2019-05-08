b743d679ae1a37b9bcaf7127e5fb2bc7b18197de
Celine Liang <cliang@Celines-MacBook-Pro.local>
2019-05-08 13:38:00 -0400

    added rouge

--- testing.py
+++ testing.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from termcolor import colored
 from knowledge_graph import KnowledgeGraph
 import util
+from rouge import Rouge
 
 def test(nlp, src, gen, verbose=False):
     if verbose:
@@ -48,6 +49,8 @@ if __name__ == "__main__":
             default='data/bottom-up/test.txt.src.tagged.shuf.400words', help='source document path (default: bottom-up)')
     parser.add_argument('--summary', dest='summary', metavar='s', type=str, 
             default='data/bottom-up/bottom_up_cnndm_015_threshold.out', help='generated summary path (default: bottom-up)')
+    parser.add_argument('--reference', dest='reference', metavar='r', type=str, 
+            default='data/bottom-up/test.txt.tgt.tagged.shuf.noslash', help='reference summary path (default: bottom-up)')
     parser.add_argument('--cache-dir', dest='cache_dir', metavar='c', type=str, 
             default='', help="directory for cached numpy entries (default: don't cache)")
     parser.add_argument('--indices', dest='indices', metavar='i', type=int, nargs='*',
@@ -70,15 +73,22 @@ if __name__ == "__main__":
     parser.add_argument('--copy-only', dest='copy_only', action='store_const',
                         const=True, default=False,
                         help='calculate average copy length only. Implies copy (default: False)')
+    parser.add_argument('--rouge', dest='rouge', action='store_const',
+                        const=True, default=False,
+                        help='calculate ROUGE scores (default: False)')
+
     args = parser.parse_args()
     document = args.document
     summary = args.summary
+    reference = args.reference
     cache_dir = args.cache_dir
     indices = args.indices
     print_scores = args.print_scores
     verbose = args.verbose
     copy = args.copy
     copy_only = args.copy_only
+    rouge = args.rouge
+
     if copy_only:
         copy = True
     draw = args.draw
@@ -89,34 +99,49 @@ if __name__ == "__main__":
         neuralcoref.add_to_pipe(nlp, greedyness=0.50, max_dist=500)
 
     scores = []
+    rouge_scores = []
     average_copy_lengths = []
+    r = Rouge()
     with open(document) as src:
         with open(summary) as gen:
-            for i, (src_line, gen_line) in enumerate(zip(src, gen)):
-                if len(indices) > 0 and i not in indices:
-                    continue
-                src_line = util.clean(src_line)
-                gen_line = util.clean(gen_line)
-                if print_scores:
-                    print(i, end = "\t")
-                if not copy_only:
-                    score = test(nlp, src_line, gen_line, verbose)
-                    scores.append(score)
+            with open(reference) as tgt:
+                for i, (src_line, gen_line, tgt_line) in enumerate(zip(src, gen, tgt)):
+                    if len(indices) > 0 and i not in indices:
+                        continue
+                    src_line = util.clean(src_line)
+                    gen_line = util.clean(gen_line)
+                    tgt_line = util.clean(tgt_line)
+
                     if print_scores:
-                        print("score:", score, end = "\t")
-                if copy:
-                    average_copy_length = util.average_copy_length(src_line, gen_line)
-                    average_copy_lengths.append(average_copy_length)
+                        print(i, end = "\t")
+                    if not copy_only:
+                        score = test(nlp, src_line, gen_line, verbose)
+                        scores.append(score)
+                        if print_scores:
+                            print("score:", score, end = "\t")
+                    if copy:
+                        average_copy_length = util.average_copy_length(src_line, gen_line)
+                        average_copy_lengths.append(average_copy_length)
+                        if print_scores:
+                            print("average copy length:", average_copy_length, end = "\t")
+                    if rouge:
+                        rouge_score = r.get_scores(gen_line, tgt_line)
+                        rouge_scores += rouge_score
+                        # if print_scores:
+                        #     print("rouge:", rouge_score, end="\t")
                     if print_scores:
-                        print("average copy length:", average_copy_length, end = "\t")
-                if print_scores:
-                    print()
+                        print()
+                    
+
 
     if cache_dir:
         if not copy_only:
             np.save(cache_dir + "scores", scores)
         if copy:
             np.save(cache_dir + "average_copy_lengths", average_copy_lengths)
+        if rouge:
+            np.save(cache_dir + "rouge", rouge_scores)
+
     
     if draw:
         sns.set()
